8c7484c6c1f7b1425bfdb1d8e3506ba22671b685
Hunter Tilley <91894350+huntert08@users.noreply.github.com>
2024-04-05 21:48:30 -0400

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Taco Salad",
+    price: 12,
+    desc: "Description of food",
+    img: "images/taco-salad.jpeg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Kale Salad",
+    price: 13,
+    desc: "Description of food",
+    img: "images/chicken-kale-salad.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Tuna Quesdilla",
+    price: 25,
+    desc: "Description of food",
+    img: "images/tuna-quesedilla.jpg",
+  },
+  /*{
+    id: "thyfhcbcv",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+  },*/
+];
